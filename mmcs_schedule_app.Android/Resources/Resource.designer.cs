5b5e6aa1dead8c7db80cdc1f8518fcaa5a54c7c1
Mikhail <mikes22@yandex.ru>
2020-05-07 00:27:33 +0300

    auto changes for build

--- mmcs_schedule_app.Android/Resources/Resource.designer.cs
+++ mmcs_schedule_app.Android/Resources/Resource.designer.cs
@@ -15,7 +15,7 @@ namespace mmcs_schedule_app.Droid
 {
 	
 	
-	[System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
 	public partial class Resource
 	{
 		
